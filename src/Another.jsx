dddd04c4fe8c9892fa95d9da937530f062b956a7
cocoabunny <65260797+cocoabunny@users.noreply.github.com>
2022-06-25 13:32:48 +1000

    First Commit

---
+++ src/Another.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Another = (props) => {
+    return (
+        <div>
+            Another Parent Component, {props.name}
+        </div>
+    )
+}
+
+export default Another
