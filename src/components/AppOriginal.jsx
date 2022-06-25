bf8eb81fe1eeab9405097827d812304af19f126c
cocoabunny <65260797+cocoabunny@users.noreply.github.com>
2022-06-25 14:45:10 +1000

    Forgot to replace my jsx files, thank you Andre ;)

--- src/components/AppOriginal.jsx
+++ src/components/AppOriginal.jsx
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react'
+import { useState } from 'react';
 import Another from './Another';
+import logo from './logo.svg';
+import '../App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   function decrement() {
-    setCount(prevcount => prevcount - 1)
+    setCount(prevCount => prevCount - 1);
   }
+
   function increment() {
-    setCount(prevcount => prevcount + 1)
+    setCount(prevCount => prevCount + 1);
   }
+
+  const someStyle = {
+    background: 'blue',
+    color: 'white',
+    fontSize: '28px',
+    fontWeight: 'bold',
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <Another name="Kanan" />
+        <Another name="Andre" />
         <div>
           <span>{count}</span>
           <button onClick={decrement}>-</button>
@@ -26,6 +34,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {true && <p style={someStyle}>{3 + 2}</p>}
         <a
           className="App-link"
           href="https://reactjs.org"
